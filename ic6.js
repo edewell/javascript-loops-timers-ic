9f862bbbab7ed48f187fc5d385433a0c91a56166
edewell <edewell3@gmail.com>
2021-09-03 08:29:15 -0500

    ic1.js

---
+++ ic6.js
@@ -0,0 +1,13 @@
+// Create a hardcoded/fixed password. Create a loop that will ask the user to enter a password until it matches the hardcoded password. Once they match alert the user `THAT’S CORRECT!!!` Keep asking the user until they get the right password.
+
+
+
+
+
+
+
+
+
+
+
+// Create a button with the text `Start Countdown!`. When the User clicks the button, display a countdown sequence in the browser as an `<h2>` from 10 to `LIFTOFF!!`. Display each number during the countdown each second (1000 ms), leading up to the word `LIFTOFF!!!`. Be sure to stop the timer once liftoff achieved.
